975911500f002a8e3417a42d84fbcf5c6a1b2675
Ayoub882 <ayoubrguig882@gmail.com>
2023-12-01 02:21:29 +0100

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    arguments = argv[1:]
+    total_sum = 0
+
+    for arg in arguments:
+        total_sum += int(arg)
+
+    print(total_sum)

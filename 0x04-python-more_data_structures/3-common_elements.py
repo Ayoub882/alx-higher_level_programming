cc08be9f229c06126f22c07eef55624971086b9b
Ayoub882 <ayoubrguig882@gmail.com>
2023-12-07 00:15:22 +0100

    Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/paython3
+def common_elements(set_1, set_2):
+    # Using the intersection method to find common elements
+    common_set = set_1.intersection(set_2)
+    return common_set

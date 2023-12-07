945b5d18c5565300ba2ef2dd55ebdd438e68dd10
Ayoub882 <ayoubrguig882@gmail.com>
2023-12-07 01:25:10 +0100

    CPython #1: PyBytesObject

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,48 @@
+#include <Python.h>
+
+/**
+ * print_python_list - prints info about Python lists
+ * @p: Python object (list)
+ */
+void print_python_list(PyObject *p)
+{
+	if (!PyList_Check(p))
+	{
+		PyErr_Print();
+		return;
+	}
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+	for (Py_ssize_t i = 0; i < PyList_Size(p); ++i)
+	{
+		printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+	}
+}
+
+/**
+ * print_python_bytes - prints info about Python bytes objects
+ * @p: Python object (bytes)
+ */
+void print_python_bytes(PyObject *p)
+{
+	if (!PyBytes_Check(p))
+	{
+		PyErr_Print();
+		return;
+	}
+
+	printf("[.] bytes object info\n");
+	printf("  size: %ld\n", PyBytes_Size(p));
+	printf("  trying string: %s\n", PyBytes_AS_STRING(p));
+
+	printf("  first %ld bytes:", PyBytes_Size(p) + 1 < 10 ? PyBytes_Size(p) + 1 : 10);
+
+	for (Py_ssize_t i = 0; i < PyBytes_Size(p) + 1 && i < 10; ++i)
+	{
+		printf(" %02x", (unsigned char)PyBytes_AS_STRING(p)[i]);
+	}
+	printf("\n");
+}

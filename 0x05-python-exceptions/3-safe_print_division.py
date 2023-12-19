534b79b6e084a32773f8e1dbb5b076ca402d6c21
Ayoub882 <ayoubrguig882@gmail.com>
2023-12-19 01:57:12 +0100

    H_M_G

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    result = None
+
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        pass  # Ignore division by zero
+    except Exception as e:
+        print("Exception:", e)
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result

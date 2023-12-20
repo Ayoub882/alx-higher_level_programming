b2ec335d7d470f089030d4833c48d50077a7afb8
Ayoub882 <ayoubrguig882@gmail.com>
2023-12-20 23:45:27 +0100

    +

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    try:
-        raise NameError(message)
-    except NameError as e:
-        print("Exception:", e)
+    raise NameError(message)

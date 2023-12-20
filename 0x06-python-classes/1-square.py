76e1e8e219a51cba8be26bfe8aa8d51ab556a1a9
Ayoub882 <ayoubrguig882@gmail.com>
2023-12-21 00:31:31 +0100

    My first square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    Square class represents a geometric square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+
+        Note:
+            This method does not perform type or value verification.
+        """
         self.__size = size
